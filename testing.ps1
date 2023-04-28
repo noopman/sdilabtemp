7871d9323829bf5cff64a1c848d2ceae3db68c01
Magnus Mårtensson <magnus@loftysoft.com>
2023-04-28 09:45:19 +0200

    testing

--- testing.ps1
+++ testing.ps1
@@ -1,6 +1,20 @@
-
-<#
 git add .
 git commit -m "testing"
 git push
-#>
+
+#gh auth login
+#gh auth login --with-token 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
+#gh workflow list
+
+gh workflow run IaC.yaml
+#gh run list --workflow=IaC.yaml
+
+return
+
+$var = az deployment group create `
+  --resource-group rg-sdilab2 `
+  --template-file 'Templates/main.bicep' `
+  --parameters 'Environments/development.parameters.json' `
+  --query "properties.outputs.swaName.value"
+
+$var.trim('"')
