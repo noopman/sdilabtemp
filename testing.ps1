eaa47095d19ec28f9ade58054228a7db6fa530fd
Magnus Mårtensson <magnus@loftysoft.com>
2023-04-28 14:15:58 +0200

    testing

--- testing.ps1
+++ testing.ps1
@@ -16,3 +16,8 @@ $var = az deployment group create `
   --template-file 'Templates/main.bicep' `
   --parameters 'Environments/development.parameters.json' `
   --query "properties.outputs.swaName.value"
+
+az staticwebapp secrets list -n stapp-sdilab-dev -g rg-sdilab-dev --query 'properties.apiKey' -o tsv 
+
+az account set --subscription 42418c1f-e6af-41ea-aa9e-41ba0fbff635
+az account show --query "name"
